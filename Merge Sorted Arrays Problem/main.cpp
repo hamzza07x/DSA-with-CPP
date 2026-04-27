53b714d672a3b634a708a956cf0d4d12c5ef8985
Muhammad Hamza <169587231+hamzza07x@users.noreply.github.com>
2026-04-27 10:50:11 +0500

    Merge Sorted Arrays Problem
    Combine two sorted arrays into one sorted array without disturbing order

---
+++ Merge Sorted Arrays Problem/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int idx = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[idx--] = nums1[i--];
+            }
+            else {
+                nums1[idx--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[idx--] = nums2[j--];
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums1 = { 1, 2, 3, 0, 0, 0 };
+    vector<int> nums2 = { 2, 5, 6 };
+    int m = 3;
+    int n = 3;
+
+    solution.merge(nums1, m, nums2, n);
+
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    return 0;
+}
